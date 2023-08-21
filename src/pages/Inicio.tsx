682879683e5fa8f94b175ab42ac0f1ea5f2f3cc1
Cristhian Botero <82228854+CBR-Byte@users.noreply.github.com>
2023-08-21 12:05:44 -0500

    carousel

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -3,7 +3,6 @@
 import {
   IonPage,
   IonTitle,
-  IonMenuButton,
   IonInput,
   IonIcon,
   IonButton,
@@ -15,18 +14,19 @@ import "./Register.css";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Inicio.css";
-import { search, home, person, mail } from "ionicons/icons/";
+import { search, home, person, mail, power } from "ionicons/icons/";
 import Card from "../components/Card/Card";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "@ionic/react/css/ionic-swiper.css";
+import {
+  Pagination,
+  Autoplay,
+  EffectCoverflow
+} from "swiper/modules";
 import 'swiper/css';
-import '@ionic/react/css/ionic-swiper.css';
-import 'swiper/css/autoplay';
-import 'swiper/css';
-import 'swiper/css/autoplay';
-import 'swiper/css/keyboard';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/zoom';
+import 'swiper/css/effect-coverflow';
+
 
 const Inicio: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -47,72 +47,88 @@ const Inicio: React.FC = () => {
 
   return (
     <IonPage>
-      <div className="cont">
+      <div className='cont'>
         <div className='headerInicio'>
-          <IonMenuButton autoHide={false} className='bot'></IonMenuButton>
           <IonTitle className='inicio'>INICIO</IonTitle>
-          <IonButton onClick={closeSesion}>Cerrar sesión</IonButton>
+          <IonButton className='bot' onClick={closeSesion}>
+            <IonIcon icon={power} />
+          </IonButton>
         </div>
         <div className='contenedor'>
           <div className='buscador'>
-            <IonIcon className='searchIcon' icon={search} />  
-            <IonInput className='inputs2' placeholder='Buscar'/>
+            <IonIcon className='searchIcon' icon={search} />
+            <IonInput className='inputs2' placeholder='Buscar' />
           </div>
           <div className='recomendados'>
-            <IonTitle className="textoRec">RECOMENDADOS</IonTitle>
-            <Swiper navigation={true} pagination={true} autoplay={true} slidesPerView={1} loop={true}>
+            <IonTitle className='textoRec'>RECOMENDADOS</IonTitle>
+            <Swiper
+              effect={'coverflow'}
+              grabCursor={true}
+              centeredSlides={true}
+              slidesPerView={'auto'}
+              coverflowEffect={{
+                rotate: 20,
+                stretch: 0,
+                depth: 50,
+                modifier: 1,
+                slideShadows: true,
+              }}
+              autoplay={{ delay: 2500, disableOnInteraction: false }}
+              pagination={true}
+              modules={[EffectCoverflow, Pagination, Autoplay]}
+            >
               <SwiperSlide>
                 <Card
-                  nombre="Juan Pablo Gómez"
-                  modalidad="Virtual"
-                  descripcion="python, java, c++, cálculo, geometría, física, química"
+                  nombre='Juan Pablo Gómez'
+                  modalidad='Virtual'
+                  descripcion='python, java, c++, cálculo, geometría, física, química'
                   calificacion={4.5}
                   precio={10000}
                   numCalificacion={10}
-                  imagen="https://www.w3schools.com/howto/img_avatar.png"
+                  imagen='https://www.w3schools.com/howto/img_avatar.png'
                 />
               </SwiperSlide>
               <SwiperSlide>
                 <Card
-                  nombre="Camila Suarez Gómez"
-                  modalidad="presencial"
-                  descripcion="python, java, c++, cálculo, geometría, física, química,inglés, español"
+                  nombre='Camila Suarez Gómez'
+                  modalidad='presencial'
+                  descripcion='python, java, c++, cálculo, geometría, física, química,inglés, español'
                   calificacion={5}
                   precio={30000}
                   numCalificacion={41}
-                  imagen="https://www.w3schools.com/w3images/avatar4.png"
+                  imagen='https://www.w3schools.com/w3images/avatar4.png'
                 />
               </SwiperSlide>
               <SwiperSlide>
                 <Card
-                  nombre="Jaime Obando Gómez"
-                  modalidad="presencial"
-                  descripcion="python, java, c++, cálculo, geometría, física, química,inglés, español"
+                  nombre='Jaime Obando Gómez'
+                  modalidad='presencial'
+                  descripcion='python, java, c++, cálculo, geometría, física, química,inglés, español'
                   calificacion={5}
                   precio={30000}
                   numCalificacion={41}
-                  imagen="https://www.w3schools.com/w3images/avatar3.png"
+                  imagen='https://www.w3schools.com/w3images/avatar3.png'
                 />
               </SwiperSlide>
             </Swiper>
           </div>
-          <div className="resultados">
-            <IonTitle className="textoRec">RESULTADOS</IonTitle>
+          <div className='resultados'>
+            <IonTitle className='textoRec'>RESULTADOS</IonTitle>
             <Card
-                  nombre="Jaime Obando Gómez"
-                  modalidad="presencial"
-                  descripcion="python, java, c++, cálculo, geometría, física, química,inglés, español"
-                  calificacion={5}
-                  precio={30000}
-                  numCalificacion={41}
-                  imagen="https://www.w3schools.com/w3images/avatar3.png"
-                />
+              nombre='Jaime Obando Gómez'
+              modalidad='presencial'
+              descripcion='python, java, c++, cálculo, geometría, física, química,inglés, español'
+              calificacion={5}
+              precio={30000}
+              numCalificacion={41}
+              imagen='https://www.w3schools.com/w3images/avatar3.png'
+            />
           </div>
         </div>
-        <div className="footerInicio">
-        <IonIcon className='iconos' icon={person} /> 
-        <IonIcon className='iconos' icon={home} /> 
-        <IonIcon className='iconos' icon={mail} /> 
+        <div className='footerInicio'>
+          <IonIcon className='iconos' icon={person} />
+          <IonIcon className='iconos' icon={home} />
+          <IonIcon className='iconos' icon={mail} />
         </div>
       </div>
     </IonPage>
